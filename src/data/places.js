f64fb4fe1dffecf3fa5f1fe81fea9b6d0964aeba
DHANRAJ <shekhawat333307@gmail.com>
2026-06-22 19:02:34 +0530

    test vercel connection

--- src/data/places.js
+++ src/data/places.js
@@ -1,7 +1,7 @@
 const places = [
   {
     id: 1,
-    name: "Ookyo",
+    name: "Tokyo",
     image: "/images/tokyo.jpg",
     short: "The city of technology, anime culture, neon lights, innovation, and endless energy.",
     description:
